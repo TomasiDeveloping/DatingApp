d055319c654bdc8440c4c360f73b66a4939ad434
Patrick Tomasi <info@tomasi-developing.ch>
2020-11-11 10:40:14 +0100

    Add routing and new bootstrap style

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {AccountService} from '../_services/account.service';
 import {Observable} from 'rxjs';
 import {User} from '../_models/user';
+import {Router} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +13,9 @@ import {User} from '../_models/user';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService,
+              private router: Router,
+              private toastr: ToastrService) { }
 
   ngOnInit(): void {
   }
@@ -20,14 +24,16 @@ export class NavComponent implements OnInit {
     console.log(this.model);
     this.accountService.login(this.model).subscribe(
       response => {
-        console.log(response);
+        this.router.navigateByUrl('/members').then();
       }, error => {
         console.log(error);
+        this.toastr.error(error.error);
       }
     );
   }
 
   logout(): void {
     this.accountService.logout();
+    this.router.navigateByUrl('/').then();
   }
 }
